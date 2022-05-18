25da8c36bf79ead7a5934eb4204ed6325304ac2e
Equipo desarrollo despegar <despegar@these.com.uy>
2022-05-18 16:00:02 -0300

    commit

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,9 +13,25 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
   @Input()hero?: Hero;
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location,
+    private menssageService : MessageService
+  ) {}
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const name = String(this.route.snapshot.paramMap.get('name'));
+    this.menssageService.add(`el heroe seleccionado ${name}`)
+    this.heroService.getHero(name)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(){
+    this.location.back()
+  }
 }
